1b6f07d70bf549e1afc5ef01ca8cb21756db9861
tobiasrummelsberger <tobias.rummelsberger@gmail.com>
2019-01-30 23:13:37 +0100

    Added Imputer (not working yet).

--- feature_preprocessing.py
+++ feature_preprocessing.py
@@ -1,17 +1,56 @@
 from sklearn.preprocessing import MultiLabelBinarizer
+from sklearn.impute import SimpleImputer
 from sklearn.externals import joblib
 import pandas as pd
+
+def depr_impute_missing_values(X_train, X_test, path):
+    categorical_features = list(X_train.select_dtypes(include=['object']))
+    numerical_features = list(X_train.select_dtypes(include=['integer', 'float']))
+
+    imputer_path = str(path+'imputer_{feature}.pkl')
+
+    ## categorical features
+
+    for feature in categorical_features:
+        imputer = SimpleImputer(strategy='most_frequent')
+        imputer.fit(X_train[feature])
+        joblib.dump(imputer, imputer_path.format(feature=feature))
+
+    for feature in categorical_features:
+        imputer = joblib.load(imputer_path.format(feature=feature))
+        X_train[feature] = imputer.fit(X_train[feature])
+
+    for feature in categorical_features:
+        imputer = joblib.load(imputer_path.format(feature=feature))
+        X_test[feature] = imputer.fit(X_test[feature])
+
+    return X_train, X_test
+
+def impute_missing_values(X_train, X_test, path):
+    categorical_features = list(X_train.select_dtypes(include=['object']))
+    numerical_features = list(X_train.select_dtypes(include=['integer', 'float']))
+
+    imputer_path = str(path+'imputer_{feature}.pkl')
+
+    imputer = SimpleImputer(strategy='most_frequent')
+
+    imputer.fit(X_train[categorical_features])
+    X_train.drop(columns=categorical_features)
+    X_train_imputed = pd.DataFrame(data=imputer.transform(X_train), index=X_train.index, columns=categorical_features)
+    X_train = pd.concat([X_train, X_train_imputed], axis=1)
+
+    return X_train, X_test
+
 
 
 def one_hot_encoding(X_train, X_test, path):
     categorical_features = list(X_train.select_dtypes(include=['object']))
 
-    binarizer_path = str(path+'binarizer{feature}.pkl')
+    binarizer_path = str(path+'binarizer_{feature}.pkl')
 
     # train binarizer
     for feature in categorical_features:
         binarizer = MultiLabelBinarizer()
-        print(X_train[feature])
         binarizer.fit(X_train[feature])
         joblib.dump(binarizer, binarizer_path.format(feature=feature))
 
