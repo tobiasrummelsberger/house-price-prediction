282a54999bfde47584ab66ccd0f5c60e11b0e33b
tobiasrummelsberger <tobias.rummelsberger@gmail.com>
2019-02-04 17:07:54 +0100

    Added functions to preprocess the different featuers individually.

--- feature_preprocessing.py
+++ feature_preprocessing.py
@@ -2,41 +2,22 @@ from sklearn.preprocessing import MultiLabelBinarizer
 from sklearn.impute import SimpleImputer
 from sklearn.externals import joblib
 import pandas as pd
-
-def depr_impute_missing_values(X_train, X_test, path):
-    categorical_features = list(X_train.select_dtypes(include=['object']))
-    numerical_features = list(X_train.select_dtypes(include=['integer', 'float']))
-
-    imputer_path = str(path+'imputer_{feature}.pkl')
-
-    ## categorical features
-
-    for feature in categorical_features:
-        imputer = SimpleImputer(strategy='most_frequent')
-        imputer.fit(X_train[feature])
-        joblib.dump(imputer, imputer_path.format(feature=feature))
-
-    for feature in categorical_features:
-        imputer = joblib.load(imputer_path.format(feature=feature))
-        X_train[feature] = imputer.fit(X_train[feature])
-
-    for feature in categorical_features:
-        imputer = joblib.load(imputer_path.format(feature=feature))
-        X_test[feature] = imputer.fit(X_test[feature])
-
-    return X_train, X_test
-
+import math
 def impute_missing_values(X_train, X_test, path):
     categorical_features = list(X_train.select_dtypes(include=['object']))
     numerical_features = list(X_train.select_dtypes(include=['integer', 'float']))
 
     imputer_path = str(path+'imputer_{feature}.pkl')
 
-    imputer = SimpleImputer(strategy='most_frequent')
+    imputer = SimpleImputer(strategy='median')
 
-    imputer.fit(X_train[categorical_features])
-    X_train.drop(columns=categorical_features)
-    X_train_imputed = pd.DataFrame(data=imputer.transform(X_train), index=X_train.index, columns=categorical_features)
+    for feature in categorical_features:
+        most_frequent_value = X_train[feature].value_counts()
+        X_train[feature].replace(to_replace=np.nan, value=most_frequent_value, inplace=True)
+
+    imputer.fit(X_train[numerical_features])
+    X_train.drop(columns=numerical_features)
+    X_train_imputed = pd.DataFrame(data=imputer.transform(X_train[numerical_features]), index=X_train.index, columns=numerical_features)
     X_train = pd.concat([X_train, X_train_imputed], axis=1)
 
     return X_train, X_test
@@ -56,7 +37,7 @@ def one_hot_encoding(X_train, X_test, path):
 
     for feature in categorical_features:
         binarizer = joblib.load(binarizer_path.format(feature=feature))
-        if len(binarizer.classes) > 1:
+        if len(binarizer.classes_) > 1:
             binarized_values = pd.DataFrame(data=binarizer.transform(X_train[feature]),
                                             index=X_train.index,
                                             columns=binarizer.classes)
@@ -82,3 +63,112 @@ def one_hot_encoding(X_train, X_test, path):
 
     return X_train, X_test
 
+def preprocess_alley(X_train, X_test):
+    is_alley = lambda x: 1 if x is str else 0
+    X_train['Alley'].fillna(value=0, inplace=True)
+    X_train['Alley'] = X_train['Alley'].apply(is_alley)
+    X_test['Alley'].fillna(value=0, inplace=True)
+    X_test['Alley'] = X_test['Alley'].apply(is_alley)
+
+    return X_train, X_test
+
+def preprocess_LotFrontage(X_train, X_test):
+    is_LotFrontage = lambda x: x if x is not math.nan else 0
+    X_train['LotFrontage'] = X_train['LotFrontage'].apply(is_LotFrontage)
+    X_train['LotFrontage'] = X_train['LotFrontage'].apply(is_LotFrontage)
+
+    return X_train, X_test
+
+def preprocess_MasVnrType(X_train, X_test):
+    X_train['MasVnrType'].fillna(value='None', inplace=True)
+    X_test['MasVnrType'].fillna(value='None', inplace=True)
+
+    return X_train, X_test
+
+def preprocess_BsmtQual(X_train, X_test):
+    X_train['BsmtQual'].fillna(value='None', inplace=True)
+    X_test['BsmtQual'].fillna(value='None', inplace=True)
+
+    return X_train, X_test
+
+def preprocess_BsmtCond(X_train, X_test):
+    X_train['BsmtCond'].fillna(value='None', inplace=True)
+    X_test['BsmtCond'].fillna(value='None', inplace=True)
+
+    return X_train, X_test
+
+def preprocess_BsmtExposure(X_train, X_test):
+    X_train['BsmtExposure'].fillna(value='None', inplace=True)
+    X_test['BsmtExposure'].fillna(value='None', inplace=True)
+
+    return X_train, X_test
+
+def preprocess_BsmtFinType1(X_train, X_test):
+    X_train['BsmtFinType1'].fillna(value='None', inplace=True)
+    X_test['BsmtFinType1'].fillna(value='None', inplace=True)
+
+    return X_train, X_test
+
+def preprocess_BsmtFinType2(X_train, X_test):
+    X_train['BsmtFinType2'].fillna(value='None', inplace=True)
+    X_test['BsmtFinType2'].fillna(value='None', inplace=True)
+
+    return X_train, X_test
+
+def preprocess_Electrical(X_train, X_test):
+    most_frequent_electrical_system = X_train['Electrical'].value_counts().keys()[0]
+    X_train['Electrical'].fillna(value=most_frequent_electrical_system, inplace=True)
+    X_test['Electrical'].fillna(value=most_frequent_electrical_system, inplace=True)
+
+    return X_train, X_test
+
+def preprocess_FireplaceQu(X_train, X_test):
+    X_train['FireplaceQu'].fillna(value='None', inplace=True)
+    X_test['FireplaceQu'].fillna(value='None', inplace=True)
+
+    return X_train, X_test
+
+
+def preprocess_GarageType(X_train, X_test):
+    X_train['GarageType'].fillna(value='None', inplace=True)
+    X_test['GarageType'].fillna(value='None', inplace=True)
+
+    return X_train, X_test
+
+
+def preprocess_GarageFinish(X_train, X_test):
+    X_train['GarageFinish'].fillna(value='None', inplace=True)
+    X_test['GarageFinish'].fillna(value='None', inplace=True)
+
+    return X_train, X_test
+
+def preprocess_GarageQual(X_train, X_test):
+    X_train['GarageQual'].fillna(value='None', inplace=True)
+    X_test['GarageQual'].fillna(value='None', inplace=True)
+
+    return X_train, X_test
+
+
+def preprocess_GarageCond(X_train, X_test):
+    X_train['GarageCond'].fillna(value='None', inplace=True)
+    X_test['GarageCond'].fillna(value='None', inplace=True)
+
+    return X_train, X_test
+
+def preprocess_PoolQC(X_train, X_test):
+    X_train['PoolQC'].fillna(value='None', inplace=True)
+    X_test['PoolQC'].fillna(value='None', inplace=True)
+
+    return X_train, X_test
+
+def preprocess_Fence(X_train, X_test):
+    X_train['Fence'].fillna(value='None', inplace=True)
+    X_test['Fence'].fillna(value='None', inplace=True)
+
+    return X_train, X_test
+
+def preprocess_MiscFeature(X_train, X_test):
+    X_train['MiscFeature'].fillna(value='None', inplace=True)
+    X_test['MiscFeature'].fillna(value='None', inplace=True)
+
+    return X_train, X_test
